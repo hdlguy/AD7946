e704e032231ad9e0e54deffda7a7e594c8226c3b
pedro <hdlguy@hotmail.com>
2020-04-05 12:19:08 -0400

    some comments

--- source/ad7946_controller.sv
+++ source/ad7946_controller.sv
@@ -1,4 +1,13 @@
-//
+// This module controls the TI AD7946 ADC with multiplexed analog input.
+// It is designed to run on the 100MHz axi clock of the Xilinx Zynq. 
+// Conversions run at the fasted reasonable rate using that clock, about 700Ksps on each analog input.
+// Since control pattern just repeats a simple modulo counter was implemented then control signals were 
+// decoded from that counter.
+
+// The pins connecting to the AD9746 should have IOB constraints in the xdc file like this.
+// set_property IOB TRUE            [get_ports {sclk[*]}]
+
+
 module ad7946_controller (
     input   logic       clk,    // 100MHz axi clock.
     output  logic       pden,
@@ -62,64 +71,6 @@ module ad7946_controller (
         end 
     end
 
-    
-//    // state machine
-//    logic[2:0] state=0;
-//    logic bit_count_reset=1, bit_count_ena=0;
-//    logic shift_ena=0, shift_latch=0;
-//    always_ff @(posedge clk) begin
-//        // defaults
-//        bit_count_reset <= 0;
-//        bit_count_ena <= 0;
-//        shift_ena <= 0;
-//        shift_latch <= 0;
-//        case (state)
-        
-//            0: begin
-//                state <= 1;
-//                pre_cs_n <= 1;
-//                bit_count_reset <= 1;
-//                pre_chsel <= 0;
-//            end
-            
-//            1: begin
-//                state <= 2;
-//                pre_chsel <= 1;
-//                bit_count_ena <= 1;
-//            end
-            
-//            2: begin
-//                state <= 3;
-//            end
-            
-//            3: begin
-//                state <= 0;
-//            end
-            
-//            default: begin
-//                state <= 0;
-//            end
-            
-//        endcase
-    
-//    end
-    
-   
-//    // bit counter
-//    logic[3:0] bit_count=15;
-//    always_ff @(posedge clk) begin
-//        if (1==bit_count_reset) begin
-//            bit_count <= 15;
-//        end else begin
-//            if (1==bit_count_ena) begin
-//                bit_count <= bit_count - 1;
-//            end
-//        end
-//    end
-    
-    
-
-    
-      
-
 endmodule
+
+
